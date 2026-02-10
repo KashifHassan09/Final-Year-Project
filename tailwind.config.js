43b6c263ae696b8171760ada705ad5707cc58db4
KashifHassan09 <kashif0909278@gmail.com>
2026-02-09 19:53:19 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
